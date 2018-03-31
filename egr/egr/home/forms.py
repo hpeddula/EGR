f4c58d74c23c701dfa307d559aa83ef8623b81d3
hpeddula <hpeddula@outlook.com>
2018-03-31 09:42:17 +0530

    Add change to forms.py

--- egr/egr/home/forms.py
+++ egr/egr/home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class NameForm(forms.Form):
-    fname = form.CharField(label='Your First name',max_length=100)
-    lname = form.CharField(label='Your Last name',max_length=100)
+    fname = forms.CharField(label='Your First name',max_length=100)
+    lname = forms.CharField(label='Your Last name',max_length=100)
